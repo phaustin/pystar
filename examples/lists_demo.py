4a95d39162f3d0395ebf3feb5d73647dcd1ddc01
Phil Austin <paustin@eos.ubc.ca>
2010-11-20 13:16:59 -0800

    prepare for merge

---
+++ examples/lists_demo.py
@@ -0,0 +1,41 @@
+"""
+
+examples at
+http://effbot.org/zone/python-list.htm
+
+Here is a python list
+
+>>> a=[1,2,3,4]
+
+indexing starts at zero
+and covers the open interval [first,last)
+
+>>> a[0:]
+[1, 2, 3, 4]
+
+>>> a[:]
+[1, 2, 3, 4]
+
+>>> a[-1]
+4
+
+index ranges are called "slices"
+
+>>> a[0:2]
+[1, 2]
+
+you can index relative to beginning or end
+
+>>> a[-2:]
+[3, 4]
+
+and slices are objects
+
+>>> endslice=slice(-2,None)
+>>> a[endslice]
+[3, 4]
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
